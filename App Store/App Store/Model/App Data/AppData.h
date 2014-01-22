a2117ed351ba54b158b532801925f19622d50f0a
vidyalaxmi <vidz_shenoy@hotmail.com>
2014-01-22 15:26:49 +0530

    Added model class for App data

---
+++ App Store/App Store/Model/App Data/AppData.h
@@ -0,0 +1,25 @@
+//
+//  AppData.h
+//  App Store
+//
+//  Created by Vidyalaxmi Shenoy on 22/01/14.
+//  Copyright (c) 2014 Vidyalaxmi Shenoy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AppData : NSObject
+
+@property (nonatomic, strong) NSString *appName;
+@property (nonatomic, strong) NSString *authorName;
+@property (nonatomic, strong) NSString *category;
+@property (nonatomic, strong) NSString *copyright;
+@property (nonatomic, strong) NSString *link;
+@property (nonatomic, strong) NSString *price;
+@property (nonatomic, strong) NSString *releaseDate;
+@property (nonatomic, strong) NSString *summary;
+@property (nonatomic, strong) NSArray  *imagePathList;
+
+-(id)initAppDataFromDictionary:(NSDictionary *)appDictionary;
+
+@end
